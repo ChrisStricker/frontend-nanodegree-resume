bf6df5d6fd34f49af492c2724cf203326ecd1b34
ChrisStricker <skaven13@sbcglobal.net>
2015-05-05 20:38:53 -0500

    additional updates

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,26 +1,3 @@
-/*
-var skills = ["programming", "tai chi"];
-var bio = {
-		"name" : HTMLheaderName.replace("%data%", "Chris Stricker"),
-		"role" : HTMLheaderRole.replace("%data%", "Web Developer"),
-		"contacts" : {
-			"home" : HTMLcontactGeneric.replace("%contact%", "home").replace("%data%", "618-555-5555"),
-			"mobile" : HTMLmobile.replace("%data%", "618-555-5555"),
-			"email" : HTMLemail.replace("%data%", "skaven13@sbcglobal.net"),
-			"twitter" : HTMLtwitter.replace("%data%", "@Skaven13"),
-			"github" : HTMLgithub.replace("%data%", "https://github.com/ChrisStricker"),
-			"location" : HTMLlocation.replace("%data%", "Swansea, Illinois")
-		},
-		"pictureUrl" : HTMLbioPic.replace("%data%", "https://avatars2.githubusercontent.com/u/11803019?v=3&s=460"),
-		"welcomeMsg" : "Our chief weapon is surprise!",
-		"skills" : skills
-	};
-
-$("#header").prepend(bio.role).prepend(bio.name).prepend(bio.pictureUrl);
-$("#header").append(bio.welcomeMsg);
-$('#topContacts').append(bio.contacts.home).append(bio.contacts.mobile).append(bio.contacts.email).append(bio.contacts.twitter).append(bio.contacts.github).append(bio.contacts.location);
-$("#main").append(skills);
-*/
 var bio = {
 		"name" : "Chris Stricker",
 		"role" : "Web Developer",
@@ -34,14 +11,35 @@ var bio = {
 		},
 		"pictureUrl" : "https://avatars2.githubusercontent.com/u/11803019?v=3&s=460",
 		"welcomeMsg" : "Our chief weapon is surprise!",
-		"skills" : ["programming", "tai chi"] 
+		"skills" : ["Java", "Perl", "HTML", "Tai Chi"],
+		"display" : function() {
+			$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role))
+				.prepend(HTMLheaderName.replace("%data%",bio.name))
+				.append(HTMLbioPic.replace("%data%",bio.pictureUrl));
+			$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg));
+			$("#topContacts").append(HTMLcontactGeneric.replace("%contact%", "home").replace("%data%",bio.contacts.home))
+				.append(HTMLmobile.replace("%data%",bio.contacts.mobile))
+				.append(HTMLemail.replace("%data%",bio.contacts.email))
+				.append(HTMLtwitter.replace("%data%",bio.contacts.twitter))
+				.append(HTMLgithub.replace("%data%",bio.contacts.github))
+				.append(HTMLlocation.replace("%data%",bio.contacts.location));
+
+			if (bio.skills.length > 0 ) {
+				var arrayLength = bio.skills.length;
+				$("#header").append(HTMLskillsStart);
+				
+				for (i = 0; i < arrayLength; i++) {
+					$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+				}
+			}			
+		}
 	};
 
 var education = { 
 		"schools" : [{
 			"name" : "Southern Illinois University",
 			"location" : "Edwardsville, IL",
-			"degreee" : "Bachelors",
+			"degree" : "Bachelors",
 			"majors" : ["Computer Science"],
 			"minors" : ["Math", "Philosophy" ],
 			"dates" : 1999
@@ -52,27 +50,119 @@ var education = {
 			"course" : "Front End Web Developer",
 			"school" : "Udacity",
 			"dates" : 2015,
-			"url" : ""
-		}]
+			"url" : "udacity.com"
+		}],
+		
+		"display" : function() {
+			for (var i = 0; i < education.schools.length; i++) {
+				$('#education').append(HTMLschoolStart);
+				$('.education-entry:last').append(HTMLschoolName.replace("%data%", education.schools[i].name))
+						.append(HTMLschoolDegree.replace("%data%", education.schools[i].degree))
+						.append(HTMLschoolDates.replace("%data%", education.schools[i].dates))
+						.append(HTMLschoolLocation.replace("%data%", education.schools[i].location))
+						.append(HTMLschoolMajor.replace("%data%", education.schools[i].majors.join(", ")));
+			}
+			
+			$('#education').append(HTMLonlineClasses);
+			for (var i = 0; i < education.online.length; i++) {
+				$('#education').append(HTMLschoolStart);
+				$('.education-entry:last').append(HTMLonlineTitle.replace("%data%", education.online[i].course))
+						.append(HTMLonlineSchool.replace("%data%", education.online[i].school))
+						.append(HTMLonlineDates.replace("%data%", education.online[i].dates))
+						.append(HTMLonlineURL.replace("%data%", education.online[i].url));
+			}			
+			
+		}
 
 	};
 
 var work = {
 		"jobs" : [{
-			"employer" : "",
-			"title" : "",
-			"dates" : "",
+			"employer" : "AT&T",
+			"title" : "Professional - Application Developer",
+			"dates" : "May 2014 to Current Date",
+			"location" : "Earth City, Missouri",
+			"description" : ""
+		},
+		{
+			"employer" : "AT&T",
+			"title" : "Senior IT Analyst",
+			"dates" : "January 2009 to May 2014",
+			"location" : "Earth City, Missouri",
+			"description" : ""
+		},
+		{
+			"employer" : "AT&T",
+			"title" : "Network Center Technician",
+			"dates" : "June 2000 to December 2008",
+			"location" : "Earth City, Missouri",
 			"description" : ""
 		}
-		]
+		],
+		"display" : function() {
+			for (job in work.jobs) {
+				$("#workExperience").append(HTMLworkStart);
+				$(".work-entry:last").append(HTMLworkEmployer.replace("%data%", work.jobs[job].employer) +
+					HTMLworkTitle.replace("%data%", work.jobs[job].title));
+				$(".work-entry:last").append(HTMLworkDates.replace("%data%", work.jobs[job].dates));
+				$(".work-entry:last").append(HTMLworkLocation.replace("%data%", work.jobs[job].location));
+				$(".work-entry:last").append(HTMLworkDescription.replace("%data%", work.jobs[job].description));	
+			}
+		}
 	};
+
+
 
 var project = { 
 		"projects": [{
-			"title": "",
-			"dates": "",
-			"description": "",
+			"title": "Portfolio",
+			"dates": "2015",
+			"description": "Portfolio website that provides links to the projects I am building in this course.",
+			"images": [ ],
+		},
+		{
+			"title": "Resume",
+			"dates": "2015",
+			"description": "Resume website that provides information on who I am and what I have done.",
 			"images": [ ],
 		}
-		]
+		],
+		"display" : function() {
+			var arrayLength = project.projects.length;
+			for (var i = 0; i < arrayLength; i++) {
+				$('#projects').append(HTMLprojectStart);
+				$('.project-entry:last').append(HTMLprojectTitle.replace("%data%", project.projects[i].title))
+					.append(HTMLprojectDates.replace("%data%", project.projects[i].dates))
+					.append(HTMLprojectDescription.replace("%data%", project.projects[i].description));
+				
+				var imgArrayLength = project.projects[i].images.length;
+				for (var j = 0; j < imgArrayLength; j++) {
+					$('.project-entry:last').append(HTMLprojectImage.replace("%data%", project.projects[i].images[j]));
+				}
+			}
+		}
 };
+
+function inName(name) {
+	var name = name.trim().split(" ");
+	return name[0] + " " + name[1].toUpperCase();
+}
+
+function locationizer() {
+    var location = [];
+    for (var i = 0; i < work.jobs.length; i++) {
+        location.push(work.jobs[i].location);
+    }
+    return location;
+}
+
+	$("#main").append(internationalizeButton);
+
+	bio.display();
+	education.display();
+	work.display();
+	project.display();
+
+	$("#mapDiv").append(googleMap);
+	initializeMap();
+
